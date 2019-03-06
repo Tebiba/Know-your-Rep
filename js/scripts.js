abb80b395ee27a12e99148ad1eecd224e220ef5a
Tebiba <Tebiba@users.noreply.github.com>
2019-03-06 14:11:09 +0300

    changes to the webpage html

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-//function myFunction() {
-  //var x = document.getElementById("governor");
-  //if (x.style.display === "none") {
-  //  x.style.display = "block";
-//  } else {
-//    x.style.display = "none";
-//  }
-//}
 
 $(document).ready(function() {
     $(".clickable").click(function() {
@@ -38,3 +30,58 @@ $(document).ready(function() {
               $("#mcas-hidden").toggle();
             });
           });
+
+
+    $(document).ready(function() {
+
+            $(".clickable-agriculture").click(function() {
+              $("#agriculture-hidden").toggle();
+              $("#agriculture-showing").toggle();
+            });
+            });
+
+            $('.clickable2').click(function() {
+              $('#variables-hidden').toggle();
+              $('#variables-showing').toggle();
+            });
+
+            $('.clickable3').click(function() {
+              $('#namingConventions-hidden').toggle();
+              $('#namingConventions-showing').toggle();
+            });
+
+            $('.clickable4').click(function() {
+              $('#functions-hidden').toggle();
+              $('#functions-showing').toggle();
+            });
+
+            $('.clickable5').click(function() {
+              $('#strings-hidden').toggle();
+              $('#strings-showing').toggle();
+            });
+
+            $('.clickable6').click(function() {
+              $('#methods-hidden').toggle();
+              $('#methods-showing').toggle();
+            });
+
+            $('.clickable7').click(function() {
+              $('#arguments-hidden').toggle();
+              $('#arguments-showing').toggle();
+            });
+
+            $('.clickable8').click(function() {
+              $('#parameters-hidden').toggle();
+              $('#parameters-showing').toggle();
+            });
+
+            $('.clickable9').click(function() {
+              $('#return-hidden').toggle();
+              $('#return-showing').toggle();
+            });
+
+            $('.clickable10').click(function() {
+              $('#chainingMethods-hidden').toggle();
+              $('#chainingMethods-showing').toggle();
+            });
+            });
